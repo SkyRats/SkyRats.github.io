2af9acad57080c001561c6af139d5a1bf6db30bb
Lucas Souza <outrolucas2002@gmail.com>
2020-08-07 19:14:30 -0300

    fix: calibrate difficulty

--- js/flappy.js
+++ js/flappy.js
@@ -80,7 +80,7 @@ function Passaro(alturaJogo) {
     window.onkeyup = e => voando = false
 
     this.animar = () => {
-        const novoY = this.getY() + (voando ? 8 : -5)
+        const novoY = this.getY() + (voando ? 10 : -6)
         const alturaMaxima = alturaJogo - this.elemento.clientHeight
 
         if (novoY <= 0) {
